f3a3c96b36e1ddd1f8550efab30554df3d1382d1
John Rees <john@bitsushi.com>
2017-04-21 13:52:08 +0100

    pow

--- SouthwarkTiles.es5.js
+++ SouthwarkTiles.es5.js
@@ -38,7 +38,7 @@ require("babel-polyfill");
   }
 })(undefined, function(L) {
   L.SouthwarkTiles = L.SouthwarkTiles || {};
-  L.SouthwarkTiles.VERSION = "0.0.3";
+  L.SouthwarkTiles.VERSION = "0.0.4";
   L.SouthwarkTiles.CRS = L.extend(
     new L.Proj
       .CRS(
@@ -46,7 +46,7 @@ require("babel-polyfill");
       "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs",
       {
         resolutions: (0, _from2.default)(new Array(12), function(x, i) {
-          return 320 / 2 ** i;
+          return 320 / Math.pow(2, i);
         })
       }
     ),
--- SouthwarkTiles.js
+++ SouthwarkTiles.js
@@ -20,14 +20,14 @@ require("babel-polyfill");
   }
 })(this, function(L) {
   L.SouthwarkTiles = L.SouthwarkTiles || {};
-  L.SouthwarkTiles.VERSION = "0.0.3";
+  L.SouthwarkTiles.VERSION = "0.0.4";
   L.SouthwarkTiles.CRS = L.extend(
     new L.Proj
       .CRS(
       "EPSG:27700",
       "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs",
       {
-        resolutions: Array.from(new Array(12), (x, i) => 320 / 2 ** i)
+        resolutions: Array.from(new Array(12), (x, i) => 320 / Math.pow(2, i))
       }
     ),
     {
